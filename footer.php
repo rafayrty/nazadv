25e3a74730d5a11c04b9f4325634cbddfda7669e
Abdul Rafay <farhatiqbal@protonmail.com>
2022-03-30 12:31:51 +0500

    Mob Footer Completed

--- footer.php
+++ footer.php
@@ -41,6 +41,83 @@
             </form>
         </div>
 
+        <div class="addresses">
+
+            <div class="address">
+
+                <div class="address__city">
+                    <h3>Nazareth, Israel</h3>
+                </div>
+
+                <div class="address__text">
+                    <p>Paulus ha Shishi 102 3rd floor</p>
+                    <a href="tel:+972 53-306-5330">+972 53-306-5330</a><br/>
+                    <a href="mailto:Info@nazadv.com">Info@nazadv.com</a><br/>
+                    <a href="#" class="view_more">View Location ></a>
+                </div>
+
+            </div>
+
+            <div class="address">
+
+<div class="address__city">
+    <h3>California, USA</h3>
+</div>
+
+<div class="address__text">
+    <p>Paulus ha Shishi 102 3rd floor</p>
+    <a href="tel:+972 53-306-5330">+972 53-306-5330</a><br/>
+    <a href="mailto:Info@nazadv.com">Info@nazadv.com</a><br/>
+    <a href="#" class="view_more">View Location ></a>
+</div>
+
+</div>
+
+<div class="footer-menu">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+    </ul>
+</div>
+
+<div class="footer-socials">
+    <p>Follow Us</p>
+
+    <ul>
+        <li>
+            
+<a href="#">
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 9C18 4.02943 13.9706 0 9 0C4.02943 0 0 4.02943 0 9C0 13.4921 3.29115 17.2155 7.59375 17.8907V11.6016H5.30859V9H7.59375V7.01719C7.59375 4.76156 8.93742 3.51562 10.9932 3.51562C11.9776 3.51562 13.0078 3.69141 13.0078 3.69141V5.90625H11.873C10.755 5.90625 10.4062 6.60006 10.4062 7.3125V9H12.9023L12.5033 11.6016H10.4062V17.8907C14.7088 17.2155 18 13.4921 18 9Z" fill="white"/>
+        </svg>
+    </a>
+    
+    </li>
+        <li>
+            <a href="#">
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 9C18 4.02943 13.9706 0 9 0C4.02943 0 0 4.02943 0 9C0 13.4921 3.29115 17.2155 7.59375 17.8907V11.6016H5.30859V9H7.59375V7.01719C7.59375 4.76156 8.93742 3.51562 10.9932 3.51562C11.9776 3.51562 13.0078 3.69141 13.0078 3.69141V5.90625H11.873C10.755 5.90625 10.4062 6.60006 10.4062 7.3125V9H12.9023L12.5033 11.6016H10.4062V17.8907C14.7088 17.2155 18 13.4921 18 9Z" fill="white"/>
+        </svg>
+    </a>
+</li>
+        <li>
+            <a href="#">
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 9C18 4.02943 13.9706 0 9 0C4.02943 0 0 4.02943 0 9C0 13.4921 3.29115 17.2155 7.59375 17.8907V11.6016H5.30859V9H7.59375V7.01719C7.59375 4.76156 8.93742 3.51562 10.9932 3.51562C11.9776 3.51562 13.0078 3.69141 13.0078 3.69141V5.90625H11.873C10.755 5.90625 10.4062 6.60006 10.4062 7.3125V9H12.9023L12.5033 11.6016H10.4062V17.8907C14.7088 17.2155 18 13.4921 18 9Z" fill="white"/>
+        </svg>
+    </a>
+</li>
+    </ul>
+</div>
+
+
+        </div>
+
+    <div class="credits">
+        <p>@ 2021 NAZADV. All right reserved</p>
+    </div>
+
 
     </div>
  
